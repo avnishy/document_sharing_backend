354cdd0213dfb2fd49a6605062a57a467ef69b6f
avnishy <avnishy@alohatechnologydev.com>
2022-03-17 23:37:54 +0530

    Initial Commit

---
+++ app/middleware/authJwt.js
@@ -0,0 +1,78 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!"
+    });
+  }
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+      res.status(403).send({
+        message: "Need Admin Role!"
+      });
+      return;
+    });
+  });
+};
+isReviewer = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "Reviewer") {
+          next();
+          return;
+        }
+      }
+      res.status(403).send({
+        message: "Need Reviewer Role!"
+      });
+    });
+  });
+};
+isReviewerOrAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "reviewer") {
+          next();
+          return;
+        }
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+      res.status(403).send({
+        message: "Need Reviewer or Admin Role!"
+      });
+    });
+  });
+};
+const authJwt = {
+  verifyToken: verifyToken,
+  isAdmin: isAdmin,
+  isReviewer: isReviewer,
+  isReviewerOrAdmin: isReviewerOrAdmin
+};
+module.exports = authJwt;
